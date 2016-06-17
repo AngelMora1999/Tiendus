76b02713df258c68cde2684cf9ca63d8e462bcc6
Angel Mora <eduardo.aemc@gmail.com>
2016-06-16 19:28:33 -0500

    Refactoring de pagos 1

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -19,36 +19,15 @@ class PaymentsController < ApplicationController
 
   def create
 
-  	payment = Payment.new({
-  		intent: "sale",
-  		payer:{
-  			payment_method: "paypal"
-  		},
-  		transactions: [
-  			{
-  				item_list: {
-  					items: [{name: "Demo",sku: :item, price: (@shopping_cart.total / 100),currency:"USD",quantity:1 }]
-  				},
-  				amount:{
-  					total: (@shopping_cart.total / 100),
-  					currency: "USD"
-  				},
-  				description: "Compra de tus productos en nuestra plataforma"
-  			}
-  		],
-  		redirect_urls: {
-  			return_url:"http://localhost:3000/checkout",
-  			cancel_url:"http://localhost:3000/carrito"
-  		}
-  		})
+      paypal_helper = Stores::Paypal.new(shopping_cart: @shopping_cart)
 
-  		if payment.create
-  			@my_payment = MyPayment.create!(paypal_id: payment.id,
+  		if paypal_helper.process_payment.create
+  			@my_payment = MyPayment.create!(paypal_id: paypal_helper.payment.id,
   										ip: request.remote_ip,
   										shopping_cart_id: cookies[:shopping_cart_id])
-  			redirect_to payment.links.find{|v| v.method == "REDIRECT" }.href
+  			redirect_to paypal_helper.payment.links.find{|v| v.method == "REDIRECT" }.href
   		else
-  			raise payment.errors.to_yaml
+  			raise paypal_helper.payment.errors.to_yaml
   		end
 
   end
