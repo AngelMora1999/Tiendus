086d72065c8472270bda69bbcef43a6ac2ac1b35
Angel Mora <eduardo.aemc@gmail.com>
2016-06-26 15:58:58 -0500

    Pagos Completados

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,35 +1,38 @@
 class PaymentsController < ApplicationController
-    
+
+  include PayPal::SDK::REST
+
   def checkout
-=begin
     @my_payment = MyPayment.find_by(paypal_id: params[:paymentId])
     if @my_payment.nil?
       redirect_to "/carrito"
     else
-      payment = Payment.find(@my_payment.paypal_id)
-      if payment.execute(payer_id: params[:PayerID])
+      Stores::Paypal.checkout(params[:PayerID],params[:paymentId]) do
         @my_payment.pay!
         redirect_to carrito_path, notice: "Se proceso el pago con PayPal"
-      else
-        redirect_to carrito_path, notice: "Hubo un error al procesar el pago"
+        return
       end
     end
-=end
-
-     @my_payment = MyPayment.find_by(paypal_id: params[:paymentId])
-      if @my_payment.nil?
-        redirect_to "/carrito"
-      else
-        Stores::Paypal.checkout(params[:PayerID],params[:paymentId]) do
-          @my_payment.pay!
-          redirect_to carrito_path, notice: "Se proceso el pago con PayPal"
-          return
-        end  
-        redirect_to carrito_path, notice: "Hubo un error al procesar el pago"
-      end
+    redirect_to carrito_path, notice: "Hubo un error al procesar el pago"
   end
 
+  def process_card
+    paypal_helper = Stores::Paypal.new(@shopping_cart.total,
+                                        @shopping_cart.items,
+                                        return_url: checkout_url,
+                                        cancel_url: carrito_url)
 
+    if paypal_helper.process_card(params).create
+      @my_payment = MyPayment.create!(paypal_id: paypal_helper.payment.id,
+                                      ip: request.remote_ip,
+                                      email: params[:email],
+                                      shopping_cart_id: cookies[:shopping_cart_id])
+      @my_payment.pay!
+      redirect_to carrito_path, notice: "El pago se realizo correctamente"
+    else
+      redirect_to carrito_path, notice: paypal_helper.payment.error
+    end
+  end
 
   def create
       paypal_helper = Stores::Paypal.new(@shopping_cart.total,
@@ -37,14 +40,14 @@ class PaymentsController < ApplicationController
                                         return_url: checkout_url,
                                         cancel_url: carrito_url)
 
-  		if paypal_helper.process_payment.create
-  			@my_payment = MyPayment.create!(paypal_id: paypal_helper.payment.id,
-  										ip: request.remote_ip,
-  										shopping_cart_id: cookies[:shopping_cart_id])
-  			redirect_to paypal_helper.payment.links.find{|v| v.method == "REDIRECT" }.href
-  		else
-  			raise paypal_helper.payment.errors.to_yaml
-  		end
+      if paypal_helper.process_payment.create
+        @my_payment = MyPayment.create!(paypal_id: paypal_helper.payment.id,
+                                    ip: request.remote_ip,
+                                    shopping_cart_id: cookies[:shopping_cart_id])
+        redirect_to paypal_helper.payment.links.find{ |v| v.method == "REDIRECT" }.href
+      else
+        raise paypal_helper.payment.error.to_yaml
+      end
 
   end
 end
