9e9695447b65b2b7988ad378339d5ff31257e4ac
Angel Mora <eduardo.aemc@gmail.com>
2016-06-28 20:41:58 -0500

    Plantilla for email

--- app/mailers/link_mailer.rb
+++ app/mailers/link_mailer.rb
@@ -3,6 +3,7 @@ class LinkMailer < ActionMailer::Base
 
 	def download_link(link)
 		@link = link
+		@product = link.product
 		mail(to: link.email,subject: "Descarga los productos que adquiriste en tiendus.com")
 	end
 end
