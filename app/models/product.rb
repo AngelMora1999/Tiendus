9b9aa916c1de0615121e650d91f7b57e7052b2ac
Angel Mora <eduardo.aemc@gmail.com>
2016-07-05 16:18:00 -0500

    Dashboard

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,6 +18,9 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :attachments
+  has_many :in_shopping_carts
+  has_one :shopping_cart, through: :in_shopping_carts
+  has_many :my_payments, through: :shopping_cart
 
   validates_presence_of :name,:user,:pricing
   validates :pricing, numericality: { greater_than: 0 }
@@ -37,4 +40,11 @@ class Product < ActiveRecord::Base
   	{name: name,sku: :item, price: (pricing / 100),currency:"USD",quantity:1 }
   end
 
+  def sales
+    my_payments.payed
+  end
+
+  def sales_total
+    my_payments.payed.count * self.pricing
+  end
 end
